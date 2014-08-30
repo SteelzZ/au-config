f60c23af491586f0074c1b62d1c507be0daddc64
Tomas Bartkus <to.bartkus@gmail.com>
2014-08-30 14:15:11 +0200

    Fix to be compatible with meteor 0.9.0

--- package.js
+++ package.js
@@ -1,28 +1,34 @@
 Package.describe({
-  summary: "AwareUnit configuration package, that provides UI to config application"
+  summary: "AwareUnit configuration package, that provides UI to configure application",
+  name: "steelzz:au-config",
+  version: "0.0.1",
+  git: "https://github.com/SteelzZ/au-config.git",
+  homepage: "https://github.com/SteelzZ/au-config"
 });
 
 Package.on_use(function (api, where) {
     // api.imply(['au-cmsinn']);
 
     api.use([
-        'underscore', 
-        'ui', 
-        'deps', 
-        'service-configuration', 
-        'templating', 
-        'iron-router', 
-        'accounts-base', 
-        'accounts-password', 
-        'accounts-ui', 
-        'roles', 
-        'accounts-github',
-        'accounts-google',
-        'accounts-facebook',
-        'accounts-twitter',
-        'accounts-bitbucket',
+        'underscore@1.0.0',
+        'ui@1.0.0',
+        'deps@1.0.0',
+        'tracker@1.0.2-rc0',
+        'service-configurations@1.0.0',
+        'templatings@1.0.0',
+        'iron:router@0.9.1',
+        'accounts-base@1.0.0',
+        'accounts-password@1.0.0',
+        'accounts-ui@1.0.0',
+        'alanning:roles@1.2.12',
+        'accounts-github@1.0.0',
+        'accounts-google@1.0.0',
+        'accounts-facebook@1.0.0',
+        'accounts-twitter@1.0.0',
+        'tarang:accounts-bitbucket@0.1.0',
     ], ['client', 'server']);
-    api.use(['jquery', 'bootstrap-3'], ['client']);
+
+    api.use(['jquery@1.0.0', 'pfafman:bootstrap-3@3.2.0'], ['client']);
 
     api.add_files([
         'lib/3rd/bootstrap-wizard/bootstrap-wizard.css',
